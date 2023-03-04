0e6fa22ba465c633bd58847acd3000edc6c59938
Kevin Amado <kamadorueda@gmail.com>
2023-03-04 10:40:03 -0700

    qr-gen

--- qr-healthlink/src/QRGenerator.js
+++ qr-healthlink/src/QRGenerator.js
@@ -1,3 +1,32 @@
+import QR from "qrcode";
+import { useState } from "react";
+
 export const QRGenerator = () => {
-  return <>QRGenerator</>;
+  const [info, setInfo] = useState("");
+  let [qr, setQr] = useState(null);
+
+  return (
+    <div>
+      <h1>QRGenerator</h1>
+      <label>
+        Information
+        <input
+          type="text"
+          value={info}
+          placeholder="info"
+          onChange={(event) => {
+            setInfo(event.target.value);
+
+            // TODO: encrypt
+            const encrypted = info;
+
+            QR.toDataURL(encrypted, (_, url) => {
+              setQr(url);
+            });
+          }}
+        />
+      </label>
+      <img src={qr} />
+    </div>
+  );
 };
